78f73c68cb6b51f265e5010c6742d5058f4bf716
Dan Reich <dkreich@gmail.com>
2019-07-11 10:31:17 -0700

    try not to run into walls

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express')
-var router  = express.Router()
+var express = require('express');
+var router  = express.Router();
+
+
+
 
 // Handle POST request to '/start'
 router.post('/start', function (req, res) {
@@ -18,17 +21,55 @@ router.post('/start', function (req, res) {
 
 // Handle POST request to '/move'
 router.post('/move', function (req, res) {
+  var moves = {};
   // NOTE: Do something here to generate your move
    //console.log(req.body)
-   let move = findFood(req.body)
+   let move = findFood(req.body);
+   let availableMoves = getAvailableMoves(req.body);
+   let taunt = 'Outta my way, snake!';
+   if (!availableMoves[move]) {
+      taunt = 'Food is too hard to find.';
+      if (availableMoves['up']) {
+        move = 'up';
+      } else if (availableMoves['left']) {
+        move = 'left';
+      } else if (availableMoves['down']) {
+        move = 'down';
+      } else {
+        move = 'right';
+      }
+   }
+
   // Response data
   var data = {
     move: move, // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    taunt, // optional, but encouraged!
   }
 
   return res.json(data)
-})
+});
+
+function getAvailableMoves(gameState) {
+  let mySnake = getMySnake(gameState);
+  let headX = mySnake.coords[0][0];
+  let headY = mySnake.coords[0][1];
+  let maxHeight = gameState.height;
+  let maxWidth = gameState.width;
+  let occupiedCoords = [];
+  snakes.each(snake => occupiedCoords.concat(snake.coords));
+  
+  return {
+    'left': (headX > 0) && isCoordOccupied(headX - 1, headY, occupiedCoords),
+    'up': (headY > 0) && isCoordOccupied(headX, headY + 1, occupiedCoords),
+    'right': (headX < maxWidth) && isCoordOccupied(headX + 1, headY, occupiedCoords),
+    'down': (headY < maxHeight) && isCoordOccupied(headX, headY + 1, occupiedCoords),
+  };
+}
+
+function isCoordOccupied(x, y, coords) {
+  return coords.find(coord => coord[0] === x && coord[1] === y);
+}
+
 function getMySnake(gameState){
   return getSnake(gameState,gameState.you)
 }
@@ -55,6 +96,7 @@ function findFood(gameState) {
         if (gameState.food[0][1] > head[1]) {
           move = "down"
         }
-        return move ;
+        return move;
 }
-module.exports = router
+
+module.exports = router;
